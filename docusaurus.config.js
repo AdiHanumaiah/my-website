c73566159be80735ba338828d46b4b2d6eb9303a
Adi <adieitc@yahoo.com>
2024-10-30 21:28:39 -0400

    config update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://adihanumaiah.github.io/',
+  url: 'https://adihanumaiah.github.io/my-website/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: 'my-website',
