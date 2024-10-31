718667156e94af0390c1d7cc3a2c7b2622f4facf
Adi <adieitc@yahoo.com>
2024-10-30 21:47:22 -0400

    push

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 const config = {
   title: 'Adi Hanumaiah',
   tagline: 'humble enthusiast of Taylor Swift',
-  favicon: 'img/favicon.ico',
+  // favicon: 'img/vinyl.svg',
 
   // Set the production url of your site here
   url: 'https://adihanumaiah.github.io/',
